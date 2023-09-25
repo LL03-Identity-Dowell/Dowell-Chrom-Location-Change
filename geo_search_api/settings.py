68d44ba3ddd78be1ac9d1d9552348269fbfaae7b
cris <crispusgikonyo@gmail.com>
2023-09-25 18:59:41 +0300

    "allowed host"

--- geo_search_api/settings.py
+++ geo_search_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$o7a&r%rqvt6zhekb+3sh@bev9(5!_6b)v9j*at!vr)uc7(0%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['74.50.67.58']
 
