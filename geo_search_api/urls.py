be92749c57e4c79c748ce6e3e2777b96e51e40fc
Nadeem Shah <nadeem.shah0777@gmail.com>
2023-10-20 22:27:51 +0500

    Update urls.py

--- geo_search_api/urls.py
+++ geo_search_api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from django.http import HttpResponse
 
 def reference_is_live(request):
-    return HttpResponse("Reference is live, please visit <a href='https://geopositioning.uxlivinglab.online/api</a>")
+    return HttpResponse("Reference is live, please visit link : https://geopositioning.uxlivinglab.online/api")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
