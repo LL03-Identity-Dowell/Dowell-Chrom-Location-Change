e2aaf36dde632110e30950877546ab17c40385c9
cris <crispusgikonyo@gmail.com>
2023-10-26 13:36:21 +0300

    Update settings

--- geo_search_api/settings.py
+++ geo_search_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$o7a&r%rqvt6zhekb+3sh@bev9(5!_6b)v9j*at!vr)uc7(0%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["geopositioning.uxlivinglab.online"]
 # API keys
 GOOGLE_API_KEY = config('GOOGLE_API_KEY', '')
 SEARCH_ENGINE_ID = config('SEARCH_ENGINE_ID','')
