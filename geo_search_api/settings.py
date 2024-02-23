0f886fdba1f17834a40926b9f2a708c1d73e9028
Chidiebere Orji Ibiam <chidiebereorjiibiam7@gmail.com>
2024-02-23 11:39:34 +0000

    Configured Redis service

--- geo_search_api/settings.py
+++ geo_search_api/settings.py
@@ -86,7 +86,8 @@ TEMPLATES = [
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Update with your Redis URL
+        'LOCATION': 'redis://redis:6379/1',  # Update with your Redis URL
+
     }
 }
 
