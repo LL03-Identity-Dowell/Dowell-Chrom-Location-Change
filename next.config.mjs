46b7f3bc14e124756b33d3de8cc1354ed148e697
hacker777-cris <crispusgikonyo@gmail.com>
2024-02-28 16:19:02 +0300

    production

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    basePath: '/Dowell-Chrome-Location-Change',
+    basePath: process.env.NODE_ENV === 'production' ? '/Dowell-Chrome-Location-Change' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Dowell-Chrome-Location-Change/' : '',
   };
   /* module.exports = nextConfig*/
   export default nextConfig;
