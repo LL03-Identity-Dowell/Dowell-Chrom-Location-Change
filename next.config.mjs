399ce81e7a25a380e1f365b3fcaf3df4ad53a322
hacker777-cris <crispusgikonyo@gmail.com>
2024-02-28 14:44:43 +0300

    remove build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/Dowell-Chrome-Location-Change', // Replace <repository-name> with your actual repository name
-  assetPrefix: '/Dowell-Chrome-Location-Change', // Replace <repository-name> with your actual repository name
-  output: 'export',
-};
-
-export default nextConfig;
+    output:'export',
+  };
+  /* module.exports = nextConfig*/
+  export default nextConfig;
+  
