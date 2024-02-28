431868f261afad6718ff28d01e5838db366de8ad
hacker777-cris <crispusgikonyo@gmail.com>
2024-02-28 15:12:44 +0300

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
+    basePath: 'Dowell-Chrome-Location-Change',
   };
   /* module.exports = nextConfig*/
   export default nextConfig;
