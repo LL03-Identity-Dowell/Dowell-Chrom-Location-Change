e8fbb87f5619db4998e13c641f387dc24169b2db
hacker777-cris <crispusgikonyo@gmail.com>
2024-02-28 09:40:13 +0300

    mjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Other configuration options
-  generateStaticParams: async () => {
-    return {
-      '/': { page: '/' },
-      // Add other routes if needed
-    };
-  },
-};
