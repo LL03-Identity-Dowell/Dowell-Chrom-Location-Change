90da33a3e3ab59c582572eee5767f37eb8e8a0f9
hacker777-cris <crispusgikonyo@gmail.com>
2024-02-28 10:43:53 +0300

    repo name

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export',
-  };
-  /* module.exports = nextConfig*/
-  export default nextConfig;
-  
+  basePath: '/Dowell-Chrome-Location-Change', // Replace <repository-name> with your actual repository name
+  assetPrefix: '/Dowell-Chrome-Location-Change', // Replace <repository-name> with your actual repository name
+  output: 'export',
+};
+
+export default nextConfig;
