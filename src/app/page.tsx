8b2526b4cf23e414aaf78c5a763f734d3a760fdc
tusharnk <tushar@sevensquaretech.com>
2024-04-30 17:20:17 +0530

    fix: form and design changes Remove country and location when we get search result Design changes according to dowell site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-'use client';
-import React from 'react';
-import { Search } from '@/components/Search/Search';
+"use client";
+import React from "react";
+import { Search } from "@/components/Search/Search";
 
 export default function Home() {
   return (
-    <div className="flex justify-betweenitems-center h-full w-full">
+    <div className="flex justify-between items-center h-full w-full">
       <Search />
     </div>
-
-  )
+  );
 }
